a7aae0a636cfb718cfe01228cdcf9c5a2d924599
TrongNhan2009 <hau123456@gmail.com>
2024-03-10 20:06:49 +0700

    initproject

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 urlpatterns = [
     path("hello/", views.hello),
     path("vaotrangchu/", views.home),
-    path('user', views.demo, name = 'demo'),
-    path('home', views.home, name = 'home'),
-    path('product', views.product, name='product'),
-    path('show', views.show, name = 'show')
+    path("col/", views.col)
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,23 +6,5 @@ def hello(request):
     return HttpResponse("hello")
 def home(request):
     return render(request, 'index.html')
-def demo(req):
-    data = [
-        {
-        'name': 'Trọng Nhân',
-        'age': 20
-        }
-    ]
-    return JsonResponse(data, safe=False)
-def product(req):
-    data = [
-        {
-            'name': 'Đăng Hán',
-            'born': '2003'
-        },
-        {
-            'name': 'Văn Tiến',
-            'born': '2003',
-        }
-    ]
-    return render(req, 'pages/product.html', {'products': data})
+def col(request):
+    return render(request, "col.html")
